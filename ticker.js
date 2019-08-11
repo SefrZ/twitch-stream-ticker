67c19c99d2805b2149d869b12430c8e7cdac4eba
Chris Simmons (SefrZ) <53380105+SefrZ@users.noreply.github.com>
2019-08-11 00:33:34 -0400

    Add files via upload

---
+++ ticker.js
@@ -0,0 +1,7 @@
+const W = document.getElementById('words_baby')
+
+let quotes = [
+  '<span class="streamer">SefrZ: </span>Making an RPG full of gay mythological creatures.'
+]
+
+W.innerHTML = quotes[0]
